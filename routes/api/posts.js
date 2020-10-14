8e89711af3838e7273a18cf7d870898cae3f99ce
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-13 22:54:09 -0400

    update api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,8 +5,18 @@ const Posts = require('../../models/Posts');
 
 //@routes POST api/posts
 //@desc create and post
-router.post('/', (req, res) => {
-    res.send(`Let's create a post!`);
+router.post('/', async (req, res) => {
+    const newPosts = new Posts(req.body);
+
+    try {
+        const post = await newPost.save()
+        if(!post) throw Error('Something went wrong while saving post');
+
+        res.status(200).json(post);
+
+    } catch(err) {
+        res.status(400).json( {msg: err} )
+    }
 });
 
 module.exports = router;

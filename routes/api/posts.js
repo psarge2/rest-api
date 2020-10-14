ade06ed7772dacea46322a0e8caeb067ba8ac008
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-13 23:14:47 -0400

    add get routes to posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,17 +3,30 @@ const router = express.Router();
 //posts Model
 const Posts = require('../../models/Posts');
 
+//@routes GET api/posts
+//@desc get all post
+
+router.get('/', async (req, res) => {
+
+    try {
+        const posts = await Posts.find();
+        if(!posts) throw Error('No Items');
+        res.status(200).json(posts);
+    } catch {
+        res.status(400).json( {msg: err} )
+    }
+});
+
+
 //@routes POST api/posts
 //@desc create and post
 router.post('/', async (req, res) => {
-    const newPosts = new Posts(req.body);
+    const newPost = new Posts(req.body);
 
     try {
-        const post = await newPost.save()
+        const post = await newPost.save();
         if(!post) throw Error('Something went wrong while saving post');
-
         res.status(200).json(post);
-
     } catch(err) {
         res.status(400).json( {msg: err} )
     }

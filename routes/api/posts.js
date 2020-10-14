21f5a9f124a8a416254e208dfb3e022aba0292ff
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-10-13 23:24:54 -0400

    add delete to posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,9 +5,7 @@ const Posts = require('../../models/Posts');
 
 //@routes GET api/posts
 //@desc get all post
-
 router.get('/', async (req, res) => {
-
     try {
         const posts = await Posts.find();
         if(!posts) throw Error('No Items');
@@ -17,17 +15,27 @@ router.get('/', async (req, res) => {
     }
 });
 
-
 //@routes POST api/posts
-//@desc create and post
+//@desc create a post
 router.post('/', async (req, res) => {
     const newPost = new Posts(req.body);
-
     try {
         const post = await newPost.save();
         if(!post) throw Error('Something went wrong while saving post');
         res.status(200).json(post);
     } catch(err) {
+        res.status(400).json( {msg: err} )
+    }
+});
+
+//@routes DELETE api/posts/:id
+//@desc delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const posts = await Posts.findByIdAndDelete(req.params.id);
+        if(!posts) throw Error('No Post Found!');
+        res.status(200).json({success : true });
+    } catch {
         res.status(400).json( {msg: err} )
     }
 });
